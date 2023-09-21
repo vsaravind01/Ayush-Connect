3f9d865964b5410dfd7d22a46255ff06fc7638c8
vsaravind01 <vsaravind01@gmail.com>
2023-09-22 00:37:43 +0530

    Connected index_router to main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,33 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from index.dependencies import get_db
+from index.router import router as index_router
+from app.database import Base, sync_engine
+
+
+Base.metadata.create_all(bind=sync_engine)
 
 app = FastAPI()
+
+app.include_router(index_router)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Ayush Connect API", "version": "0.1.0"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/healthcheck", status_code=status.HTTP_200_OK)
+async def perform_healthcheck(db=Depends(get_db)):
+    result = db.exec("SELECT 1")
+    one = result.one()
+    if not one[0] == 1:
+        db_status = False
+    else:
+        db_status = True
+
+    status = {}
+    if not db_status:
+        raise HTTPException(status_code=404, detail="db conn not available")
+    elif db_status:
+        status["operations"] = "ok"
+    return status

294fe32a820f459aea6fe215e8f91d7eadce0f5b
vsaravind01 <vsaravind01@gmail.com>
2023-11-03 17:42:40 +0530

    Use Black

--- backend/app/index/plants/router.py
+++ backend/app/index/plants/router.py
@@ -1,7 +1,8 @@
-from app.index.utils.es import ElasticSearchClient
-from fastapi import APIRouter, Body, Depends, Query
+from fastapi import APIRouter, Body, Query
 from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
+
+from app.index.utils.es import ElasticSearchClient
 
 es_client = ElasticSearchClient()
 
@@ -9,8 +10,9 @@ router = APIRouter(prefix="/plants", tags=["index"])
 
 
 @router.get("/")
-async def get_plants(page: int = Query(1, ge=1),
-                     size: int = Query(20, ge=1, le=50)) -> JSONResponse:
+async def get_plants(
+    page: int = Query(1, ge=1), size: int = Query(20, ge=1, le=50)
+) -> JSONResponse:
     """
     Get all plants from plants index
 
@@ -24,20 +26,19 @@ async def get_plants(page: int = Query(1, ge=1),
     - **JSONResponse**: JSON response with all plants
     """
     client = es_client.client
-    response = client.search(index="plants",
-                             size=size,
-                             from_=(page - 1) * size,
-                             query={"match_all": {}})
+    response = client.search(
+        index="plants", size=size, from_=(page - 1) * size, query={"match_all": {}}
+    )
     return JSONResponse(status_code=200, content=response.body)
 
 
 @router.get("/autocomplete")
 async def autocomplete_plants(
-        query: str = Query(..., min_length=1),
-        field: str = Query(
-            "scientific_name.autocomplete"),
-        page: int = Query(1, ge=1),
-        size: int = Query(20, ge=1, le=50)) -> JSONResponse:
+    query: str = Query(..., min_length=1),
+    field: str = Query("scientific_name.autocomplete"),
+    page: int = Query(1, ge=1),
+    size: int = Query(20, ge=1, le=50),
+) -> JSONResponse:
     """
     Autocomplete plants from plants index
 
@@ -59,24 +60,20 @@ async def autocomplete_plants(
         suggest={
             "autocomplete_suggest": {
                 "text": query,
-                "completion": {
-                    "field": field,
-                    "skip_duplicates": True,
-                    "size": 50
-                }
+                "completion": {"field": field, "skip_duplicates": True, "size": 50},
             }
-        }
+        },
     )
     return JSONResponse(status_code=200, content=response.body)
 
 
 @router.get("/search")
 async def search_plants(
-        query: str = Query(..., min_length=3),
-        fields: str = Query(
-            "generic_name,scientific_name,accepted_scientific_name"),
-        page: int = Query(1, ge=1),
-        size: int = Query(20, ge=1, le=50)) -> JSONResponse:
+    query: str = Query(..., min_length=3),
+    fields: str = Query("generic_name,scientific_name,accepted_scientific_name"),
+    page: int = Query(1, ge=1),
+    size: int = Query(20, ge=1, le=50),
+) -> JSONResponse:
     """
     Search plants from plants index
 
@@ -96,10 +93,8 @@ async def search_plants(
         index="plants",
         size=size,
         from_=(page - 1) * size,
-        query={"multi_match": {
-            "query": query,
-            "fields": fields
-        }})
+        query={"multi_match": {"query": query, "fields": fields}},
+    )
     return JSONResponse(status_code=200, content=response.body)
 
 
@@ -145,20 +140,18 @@ async def add_plants(plant_data: dict = Body(...)) -> JSONResponse:
 
         if response:
             return JSONResponse(
-                status_code=201,
-                content={
-                    "message": "Plant records added successfully"
-                })
+                status_code=201, content={"message": "Plant records added successfully"}
+            )
         else:
             return JSONResponse(
                 status_code=500,
-                content={
-                    "message": "Failed to add plant records to the index"
-                })
+                content={"message": "Failed to add plant records to the index"},
+            )
     except Exception as e:
         raise HTTPException(
             status_code=500,
-            detail=f"Plant record addition failed - Internal Server Error: {str(e)}")
+            detail=f"Plant record addition failed - Internal Server Error: {str(e)}",
+        )
 
 
 @router.put("/{plant_id}")
@@ -187,19 +180,17 @@ async def update_plant(plant_id: str, plant_data: dict = Body(...)) -> JSONRespo
         if response:
             return JSONResponse(
                 status_code=200,
-                content={
-                    "message": "Plant record updated successfully"
-                })
+                content={"message": "Plant record updated successfully"},
+            )
         else:
             return JSONResponse(
-                status_code=500,
-                content={
-                    "message": "Failed to update plant record"
-                })
+                status_code=500, content={"message": "Failed to update plant record"}
+            )
     except Exception as e:
         raise HTTPException(
             status_code=500,
-            detail=f"Plant record update failed - Internal Server Error: {str(e)}")
+            detail=f"Plant record update failed - Internal Server Error: {str(e)}",
+        )
 
 
 @router.delete("/{plant_id}")
@@ -219,13 +210,9 @@ async def delete_plant(plant_id: str) -> JSONResponse:
 
     if response:
         return JSONResponse(
-            status_code=200,
-            content={
-                "message": "Plant deleted successfully"
-            })
+            status_code=200, content={"message": "Plant deleted successfully"}
+        )
     else:
         return JSONResponse(
-            status_code=404,
-            content={
-                "message": "Plant not found or deletion failed"
-            })
+            status_code=404, content={"message": "Plant not found or deletion failed"}
+        )
